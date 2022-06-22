140b135be13bedb78c78b7db98175a65ccb72323
Kevin Kim <kevinkim0522@gmail.com>
2022-06-21 22:16:18 -0400

    Added tutorial scene and button

--- Digital Prototyping Module (Non-Immersive)/Assets/Scripts/MainMenu.cs
+++ Digital Prototyping Module (Non-Immersive)/Assets/Scripts/MainMenu.cs
@@ -7,9 +7,13 @@ public class MainMenu : MonoBehaviour
 {
 
     public void LoadQuiz (int buttonIndex) {
-        Debug.Log(buttonIndex);
         QuizManager.categoryIndex = buttonIndex;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         // [equivalent to] SceneManager.LoadScene("Game");
+    }
+
+    public void LoadTutorial () {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        // [equivalent to] SceneManager.LoadScene("Tutorial");
     }
 }
